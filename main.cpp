be7a7c536384ebb8cc8d1040730231cf293ff7f0
Andy Teijelo Pérez <ateijelo@gmail.com>
2015-11-11 09:12:03 -0500

    Change icon

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     //app.setApplicationVersion("14.02");
 
     MainWindow m;
+    m.setWindowIcon(QIcon(":/img/icon.svg"));
     m.show();
 
     return app.exec();
